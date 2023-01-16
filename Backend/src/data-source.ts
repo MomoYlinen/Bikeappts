f091eecbd07f42f6398e90f3a6cc1737558aba35
MomoYlinen <momo.ylinen@outlook.com>
2023-01-16 20:02:15 +0200

    .env added

--- Backend/src/data-source.ts
+++ Backend/src/data-source.ts
@@ -2,14 +2,17 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Stations } from "./entity/Stations"
 import { Trips } from "./entity/Trips"
+import dotenv from  "dotenv"
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.HOST,
     port: 3306,
-    username: "root",
-    password: "MMolumir3",
-    database: "bikeappdb",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     synchronize: false,
     logging: false,
     entities: [Trips,Stations],
