11a30d79d9e4f83fd49a5297bd96ca93cb7958fa
MomoYlinen <momo.ylinen@outlook.com>
2023-01-16 19:54:12 +0200

    Typeorm and Routes added for express app

--- Backend/src/Routes/StationRoute.ts
+++ Backend/src/Routes/StationRoute.ts
@@ -1,12 +1,13 @@
-import express from "express";
+import express, {Request,Response} from "express";
 import { Stations } from "../entity/Stations";
 import 'reflect-metadata'
 import { AppDataSource } from "../data-source";
 const router = express.Router();
 
+const StationsDB = AppDataSource.getRepository(Stations)
+
 router.get(
   "/",(async (req, res) => {
-    const StationsDB = AppDataSource.getRepository(Stations)
     try {
       const stations = await StationsDB.find();
       res.status(200).json(stations);
@@ -17,7 +18,14 @@ router.get(
 );
 
 router.get(
-    "/:id",(async (req, res) => {})
+    "/:id",(async (req:Request, res:Response) => {
+      try {
+        const stations = await StationsDB.findOne({where: {id: parseInt(req.params.id, 10)}})
+        res.status(200).json(stations);
+      }catch (err) {
+        console.log(err);
+      }
+    })
   );
 
 export default router;
--- Backend/src/Routes/TripRoute.ts
+++ Backend/src/Routes/TripRoute.ts
@@ -1,12 +1,14 @@
-import express from "express";
+import express, {Request,Response} from "express";
+import { QueryBuilder } from "typeorm";
 import { AppDataSource } from "../data-source"
 import 'reflect-metadata'
 import { Trips } from "../entity/Trips";
 const router = express.Router();
 
+const TripsDB = AppDataSource.getRepository(Trips);
+
 router.get(
-  "/",(async (req, res) => {
-    const TripsDB = AppDataSource.getRepository(Trips);
+  "/",(async (req:Request, res:Response) => {
     try {
       const trips = await TripsDB.find();
       res.status(200).json(trips);
@@ -17,7 +19,25 @@ router.get(
 );
 
 router.get(
-    "/:id",(async (req, res) => {})
-  );
+  "/departurestation/:id",(async (req:Request, res:Response) => {
+    try {
+      const departurestation = await TripsDB.createQueryBuilder("trips").where("trips.departurestationID = :id", { id: parseInt(req.params.id) }).limit(50).getRawMany()
+      res.status(200).send(departurestation);
+    }catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+router.get(
+  "/returnstation/:id",(async (req:Request, res:Response) => {
+    try {
+      const returnstation = await TripsDB.createQueryBuilder("trips").where("trips.returnstationID = :id", { id: parseInt(req.params.id) }).limit(50).getRawMany()
+      res.status(200).json(returnstation);
+    }catch (err) {
+      console.log(err);
+    }
+  })
+);
 
 export default router;
