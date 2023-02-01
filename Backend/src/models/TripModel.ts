4df4eeb260134b54984d1c536160e0409adc0ce1
MomoYlinen <momo.ylinen@outlook.com>
2023-02-01 17:13:05 +0200

    Backend ready!

--- Backend/src/models/TripModel.ts
+++
@@ -1,8 +0,0 @@
-import { AppDataSource } from "../data-source"
-import 'reflect-metadata'
-import { Trips } from "../entity/Trips";
-
-export function getTripRepo (){
-    const getRepo = AppDataSource.getRepository(Trips)
-    return getRepo
-}
